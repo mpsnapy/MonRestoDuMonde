95bdfb62e8da7b43dd5f2d7ab60a180be2f2ea16
Matthieu PLAINEAU <MP@MPS-MacBook-Pro.local>
2018-09-20 17:30:45 +0200

    Class Plat

---
+++ MonRestoDuMonde/Modèle/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
---
+++ MonRestoDuMonde/Modèle/Plat.swift
@@ -0,0 +1,40 @@
+//
+//  Plat.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import UIKit
+
+class Plat {
+    
+    private var _type: Type
+    private var _pays: String
+    private var _desc: String
+    private var _image: UIImage
+    
+    var type: Type {
+        return _type
+    }
+    
+    var pays: String {
+        return _pays
+    }
+    
+    var desc: String {
+        return _desc
+    }
+    
+    var image: UIImage {
+        return _image
+    }
+    
+    init(type: Type, pays: String, desc: String, image: UIImage) {
+        self._type = type
+        self._pays = pays
+        self._desc = desc
+        self._image = image
+    }
+}
