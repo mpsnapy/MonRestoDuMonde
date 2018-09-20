9bfb9b5927ee3ae72b1a3e061292dda70b71ed18
Matthieu PLAINEAU <MP@MPS-MacBook-Pro.local>
2018-09-20 16:57:44 +0200

    Ajout CollectionView

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
