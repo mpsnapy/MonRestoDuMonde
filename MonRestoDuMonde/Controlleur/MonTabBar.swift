8bb8d54aa82dbc56c84debd9169565d24ba2b611
Matthieu PLAINEAU <MP@MPS-MacBook-Pro.local>
2018-09-20 15:11:55 +0200

    TabBarCustom

---
+++ MonRestoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
