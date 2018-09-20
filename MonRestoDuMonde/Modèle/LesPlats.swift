a8338694836ec97d1222382d87c7b24dfa33d7e6
Matthieu PLAINEAU <MP@MPS-MacBook-Pro.local>
2018-09-20 18:00:13 +0200

    Menu

---
+++ MonRestoDuMonde/Modèle/LesPlats.swift
@@ -0,0 +1,28 @@
+//
+//  LesPlats.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import UIKit
+
+class LesPlats {
+    
+    static let obtenir = LesPlats()
+    
+    func lesMenus() -> [Menu] {
+        var menus = [Menu]()
+        let it = Menu(pays: ITALIE, image: #imageLiteral(resourceName: "pizza"))
+        let jap = Menu(pays: JAPON, image: #imageLiteral(resourceName: "sushis"))
+        let chi = Menu(pays: CHINE, image: #imageLiteral(resourceName: "crevette aigre doux"))
+        let usa = Menu(pays: USA, image: #imageLiteral(resourceName: "burger"))
+        let mex = Menu(pays: MEXIQUE, image: #imageLiteral(resourceName: "fajitas"))
+        let esp = Menu(pays: ESPAGNE, image: #imageLiteral(resourceName: "paella"))
+        let sui = Menu(pays: SUISSE, image: #imageLiteral(resourceName: "fondue"))
+        menus.append(contentsOf: [it, jap, chi, usa, mex, esp, sui])
+        
+        return menus
+    }
+}
---
+++ MonRestoDuMonde/Modèle/Menu.swift
@@ -0,0 +1,34 @@
+//
+//  Menu.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PLAINEAU on 20/09/2018.
+//  Copyright © 2018 Matthieu PLAINEAU. All rights reserved.
+//
+
+import UIKit
+
+class Menu {
+    
+    private var _pays: String
+    private var _image: UIImage
+    private var _plats: [Plat]
+    
+    var pays: String {
+        return _pays
+    }
+    
+    var image: UIImage {
+        return _image
+    }
+    
+    var plats: [Plat] {
+        return _plats
+    }
+    
+    init(pays: String, image: UIImage) {
+        self._pays = pays
+        self._image = image
+        self._plats = [Plat]()
+    }
+}
